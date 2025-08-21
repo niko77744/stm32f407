dc64d4ecf5b2509ff437ebb4dd231c45b68c3935
niko77744 <1836045035@qq.com>
2025-08-21 22:21:23 +0800

    添加三方库

--- User/w25qxx.c
+++ User/w25qxx.c
@@ -23,6 +23,33 @@ uint16_t w25qxx_read_id(void)
     return id;
 }
 
+//[SFUD](../Lib_SFUD/src/sfud.c:116) 开始初始化通用串行闪存驱动程序（SFUD）V1.1.0。
+//[SFUD](../Lib_SFUD/src/sfud.c:117) 您可以在 https://github.com/armink/SFUD 获取最新版本。
+//[SFUD](../Lib_SFUD/src/sfud.c:883) 闪存设备制造商 ID 为 0xEF，内存类型 ID 为 0x40，容量 ID 为 0x18。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:132) 检查 SFDP 头部正常。版本为 V1.0，NPN 为 0。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:175) 检查 JEDEC 基本闪存参数头部正常。表 ID 为 0，版本为 V1.0，长度为 9，参数表指针为 0x000080。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：203) JEDEC 基本闪存参数表信息：
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：204) MSB-LSB  3    2    1    0
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：207) [0001] 0xFF 0xF1 0x20 0xE5
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：207) [0002] 0x07 0xFF 0xFF 0xFF
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：207) [0003] 0x6B 0x08 0xEB 0x44
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：207) [0004] 0xBB 0x42 0x3B 0x08
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：207) [0005] 0xFF 0xFF 0xFF 0xFE
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：207) [0006] 0x00 0x00 0xFF 0xFF[SFUD](../Lib_SFUD/src/sfud_sfdp.c:207) [0007] 0xEB 0x21 0xFF 0xFF
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:207) [0008] 0x52 0x0F 0x20 0x0C
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:207) [0009] 0x00 0x00 0xD8 0x10
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:215) 整个设备支持 4KB 擦除。命令为 0x20。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:234) 写入粒度为 64 字节或更大。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:245) 目标闪存状态寄存器是非易失性的。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:271) 仅支持 3 字节寻址。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:305) 容量为 16777216 字节。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c:312) 闪存设备支持 4KB 块擦除。命令为 0x20。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：312) 闪存设备支持 32KB 块擦除。命令为 0x52。
+//[SFUD](../Lib_SFUD/src/sfud_sfdp.c：312) 闪存设备支持 64KB 块擦除。命令为 0xD8。
+//[SFUD]发现了一颗旺宏闪存芯片。大小为 16777216 字节。
+//[SFUD](../Lib_SFUD/src/sfud.c：861) 闪存设备复位成功。
+//[SFUD]W25Q128BV 闪存设备初始化成功。
+
 static void sfud_demo(uint32_t addr, uint32_t size, uint8_t *data)
 {
     sfud_err result = SFUD_SUCCESS;
