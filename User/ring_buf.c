9696554040c46e1698ef46ec1f470cb116b599e5
niko77744 <1836045035@qq.com>
2025-08-23 00:17:23 +0800

    添加环形缓冲区

---
+++ User/ring_buf.c
@@ -0,0 +1,11 @@
+#define LOG_TAG "ring_buf"
+#include "ring_buf.h"
+#include "elog.h"
+#include "lwrb.h"
+
+// lwrb_t ring_buffer;
+
+/*
+https://zhuanlan.zhihu.com/p/146313378
+https://blog.csdn.net/qq_36075612/article/details/116144349
+*/
---
+++ User/ring_buf.h
@@ -0,0 +1,6 @@
+#ifndef __RING_BUF_H__
+#define __RING_BUF_H__
+
+#include "main.h"
+
+#endif /* __RING_BUF_H__ */
