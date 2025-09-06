5a5a1b6c5facef283ee39d077f585d0f6f5e59f1
niko77744 <1836045035@qq.com>
2025-09-07 00:42:32 +0800

    修改rtos配置

--- Third_Party/FreeRTOS-Kernel/FreeRTOSConfig.h
+++ Third_Party/FreeRTOS-Kernel/FreeRTOSConfig.h
@@ -44,51 +44,74 @@
 #include <stdint.h>
 extern uint32_t SystemCoreClock;
 
-#define configUSE_PREEMPTION 1
-#define configUSE_IDLE_HOOK 1
-#define configUSE_TICK_HOOK 1
-#define configCPU_CLOCK_HZ (SystemCoreClock)
-#define configTICK_RATE_HZ ((TickType_t)1000)
-#define configMAX_PRIORITIES (5)
-#define configMINIMAL_STACK_SIZE ((unsigned short)130)
-#define configTOTAL_HEAP_SIZE ((size_t)(30 * 1024))
-#define configMAX_TASK_NAME_LEN (10)
-#define configUSE_TRACE_FACILITY 1
-#define configUSE_16_BIT_TICKS 0
-#define configIDLE_SHOULD_YIELD 0
-#define configUSE_MUTEXES 1
-#define configQUEUE_REGISTRY_SIZE 8
-#define configCHECK_FOR_STACK_OVERFLOW 2
-#define configUSE_RECURSIVE_MUTEXES 1
-#define configUSE_MALLOC_FAILED_HOOK 1
-#define configUSE_APPLICATION_TASK_TAG 0
-#define configUSE_COUNTING_SEMAPHORES 1
-#define configGENERATE_RUN_TIME_STATS 0
-#define configUSE_QUEUE_SETS 1         // 为1时启用队列集功能
-#define configUSE_TASK_NOTIFICATIONS 1 // 使用任务通知功能
-// #define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
+#define configUSE_PREEMPTION 1                         /* 1: 抢占式调度器, 0: 协程式调度器, 无默认需定义 */
+#define configUSE_IDLE_HOOK 1                          /* 1: 使能空闲任务钩子函数, 无默认需定义  */
+#define configUSE_TICK_HOOK 1                          /* 1: 使能系统时钟节拍中断钩子函数, 无默认需定义 */
+#define configUSE_DAEMON_TASK_STARTUP_HOOK 0           /* 1: 使能定时器服务任务首次执行前的钩子函数, 默认: 0 */
+#define configCPU_CLOCK_HZ (SystemCoreClock)           /* 定义CPU主频, 单位: Hz, 无默认需定义 */
+#define configTICK_RATE_HZ ((TickType_t)1000)          /* 定义系统时钟节拍频率, 单位: Hz, 无默认需定义 */
+#define configMAX_PRIORITIES (5)                       /* 定义最大优先级数, 最大优先级=configMAX_PRIORITIES-1, 无默认需定义 */
+#define configMINIMAL_STACK_SIZE ((unsigned short)128) /* 定义空闲任务的栈空间大小, 单位: Word, 无默认需定义 */
+#define configTOTAL_HEAP_SIZE ((size_t)(64 * 1024))    /* FreeRTOS堆中可用的RAM总量, 单位: Byte, 无默认需定义 */
+#define configMAX_TASK_NAME_LEN (10)                   /* 定义任务名最大字符数, 默认: 16 */
+#define configUSE_TRACE_FACILITY 1                     /* 1: 使能可视化跟踪调试, 默认: 0 */
+#define configUSE_16_BIT_TICKS 0                       /* 1: 定义系统时钟节拍计数器的数据类型为16位无符号数, 无默认需定义 */
+#define configIDLE_SHOULD_YIELD 0                      /* 1: 使能在抢占式调度下,同优先级的任务能抢占空闲任务, 默认: 1 */
+#define configUSE_MUTEXES 1                            /* 1: 使能互斥信号量, 默认: 0 */
+#define configQUEUE_REGISTRY_SIZE 8                    /* 定义可以注册的信号量和消息队列的个数, 默认: 0 */
+#define configCHECK_FOR_STACK_OVERFLOW 2               /* 1: 使能栈溢出检测方法1, 2: 使能栈溢出检测方法2, 默认: 0 */
+#define configUSE_RECURSIVE_MUTEXES 1                  /* 1: 使能递归互斥信号量, 默认: 0 */
+#define configUSE_MALLOC_FAILED_HOOK 1                 /* 1: 使能动态内存申请失败钩子函数, 默认: 0 */
+#define configUSE_COUNTING_SEMAPHORES 1                /* 1: 使能计数信号量, 默认: 0 */
+#define configGENERATE_RUN_TIME_STATS 0                /* 1: 使能任务运行时间统计功能, 默认: 0 */
+#define configUSE_QUEUE_SETS 1                         /* 1: 使能队列集, 默认: 0 */
+#define configUSE_TASK_NOTIFICATIONS 1                 /* 1: 使能任务间直接的消息传递,包括信号量、事件标志组和消息邮箱, 默认: 1 */
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES 1        /* 定义任务通知数组的大小, 默认: 1 */
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 1      /* 1: 使用硬件计算下一个要运行的任务, 0: 使用软件算法计算下一个要运行的任务, 默认: 0 */
+#define configUSE_TICKLESS_IDLE 0                      /* 1: 使能tickless低功耗模式, 默认: 0 */
+#define configUSE_TIME_SLICING 1                       /* 1: 使能时间片调度, 默认: 1 */
+#define configUSE_NEWLIB_REENTRANT 0                   /* 1: 任务创建时分配Newlib的重入结构体, 默认: 0 */
+#define configENABLE_BACKWARD_COMPATIBILITY 0          /* 1: 使能兼容老版本, 默认: 1 */
+#define configUSE_APPLICATION_TASK_TAG 0               /* */
+
+/* 内存分配相关定义 */
+#define configSUPPORT_STATIC_ALLOCATION 0  /* 1: 支持静态申请内存, 默认: 0 */
+#define configSUPPORT_DYNAMIC_ALLOCATION 1 /* 1: 支持动态申请内存, 默认: 1 */
+#define configAPPLICATION_ALLOCATED_HEAP 1 /* 1: 用户手动分配FreeRTOS内存堆(ucHeap), 默认: 0 */
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 0
-#define configMAX_CO_ROUTINE_PRIORITIES (2)
+#define configUSE_CO_ROUTINES 0             /* 1: 启用协程, 默认: 0 */
+#define configMAX_CO_ROUTINE_PRIORITIES (2) /* 定义协程的最大优先级, 最大优先级=configMAX_CO_ROUTINE_PRIORITIES-1, 无默认configUSE_CO_ROUTINES为1时需定义 */
 
 /* Software timer definitions. */
-#define configUSE_TIMERS 1
-#define configTIMER_TASK_PRIORITY (2)
-#define configTIMER_QUEUE_LENGTH 10
-#define configTIMER_TASK_STACK_DEPTH (configMINIMAL_STACK_SIZE * 2)
+#define configUSE_TIMERS 1                                          /* 1: 使能软件定时器, 默认: 0 */
+#define configTIMER_TASK_PRIORITY (2)                               /* 定义软件定时器任务的优先级, 无默认configUSE_TIMERS为1时需定义 */
+#define configTIMER_QUEUE_LENGTH 10                                 /* 定义软件定时器命令队列的长度, 无默认configUSE_TIMERS为1时需定义 */
+#define configTIMER_TASK_STACK_DEPTH (configMINIMAL_STACK_SIZE * 2) /* 定义软件定时器任务的栈空间大小, 无默认configUSE_TIMERS为1时需定义 */
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet 1
-#define INCLUDE_uxTaskPriorityGet 1
-#define INCLUDE_vTaskDelete 1
+#define INCLUDE_vTaskPrioritySet 1            /* 设置任务优先级 */
+#define INCLUDE_uxTaskPriorityGet 1           /* 获取任务优先级 */
+#define INCLUDE_vTaskDelete 1                 /* 删除任务 */
+#define INCLUDE_xResumeFromISR 1              /* 恢复在中断中挂起的任务 */
+#define INCLUDE_vTaskSuspend 1                /* 挂起任务 */
+#define INCLUDE_vTaskDelayUntil 1             /* 任务绝对延时 */
+#define INCLUDE_vTaskDelay 1                  /* 任务延时 */
+#define INCLUDE_xTaskGetSchedulerState 1      /* 获取任务调度器状态 */
+#define INCLUDE_xTaskGetCurrentTaskHandle 1   /* 获取当前任务的任务句柄 */
+#define INCLUDE_uxTaskGetStackHighWaterMark 1 /* 获取任务堆栈历史剩余最小值 */
+#define INCLUDE_xTaskGetIdleTaskHandle 1      /* 获取空闲任务的任务句柄 */
+#define INCLUDE_eTaskGetState 1               /* 获取任务状态 */
+#define INCLUDE_xEventGroupSetBitFromISR 1    /* 在中断中设置事件标志位 */
+#define INCLUDE_xTimerPendFunctionCall 1      /* 将函数的执行挂到定时器服务任务 */
+#define INCLUDE_xTaskAbortDelay 1             /* 中断任务延时 */
+#define INCLUDE_xTaskGetHandle 1              /* 通过任务名获取任务句柄 */
+#define INCLUDE_xTaskResumeFromISR 1          /* 恢复在中断中挂起的任务 */
 #define INCLUDE_vTaskCleanUpResources 1
-#define INCLUDE_vTaskSuspend 1
-#define INCLUDE_vTaskDelayUntil 1
-#define INCLUDE_vTaskDelay 1
 
 /* Cortex-M specific definitions. */
+/* 中断嵌套行为配置 */
 #ifdef __NVIC_PRIO_BITS
 /* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
 #define configPRIO_BITS __NVIC_PRIO_BITS
@@ -98,13 +121,13 @@ to exclude the API function. */
 
 /* The lowest interrupt priority that can be used in a call to a "set priority"
 function. */
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 0xf
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 0xf /* 中断最低优先级 */
 
 /* The highest interrupt priority that can be used by any interrupt service
 routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
 INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
 PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5 /* FreeRTOS可管理的最高中断优先级 */
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
@@ -115,18 +138,25 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
-// #define configASSERT(x)           \
-//     if ((x) == 0)                 \
-//     {                             \
-//         taskDISABLE_INTERRUPTS(); \
-//         for (;;)                  \
-//             ;                     \
-//     }
+// #define configASSERT(x) \
+//     if ((x) == 0)       \
+//     vAssertCalled(__FILE__, __LINE__)
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names. */
 #define vPortSVCHandler SVC_Handler
 #define xPortPendSVHandler PendSV_Handler
 // #define xPortSysTickHandler SysTick_Handler
+
+/* FreeRTOS MPU 特殊定义 */
+// #define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0
+// #define configTOTAL_MPU_REGIONS                                8
+// #define configTEX_S_C_B_FLASH                                  0x07UL
+// #define configTEX_S_C_B_SRAM                                   0x07UL
+// #define configENFORCE_SYSTEM_CALLS_FROM_KERNEL_ONLY            1
+// #define configALLOW_UNPRIVILEGED_CRITICAL_SECTIONS             1
+
+/* ARMv8-M 安全侧端口相关定义。 */
+// #define secureconfigMAX_SECURE_CONTEXTS         5
 
 #endif /* FREERTOS_CONFIG_H */
